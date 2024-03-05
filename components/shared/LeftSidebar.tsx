6f367c98b5024d78f81f04ad0f072591e6c80d19
kkunal026 <kunalakakk@gmail.com>
2024-03-06 04:21:39 +0530

    Navbar done now working on leftsidebar

--- components/shared/LeftSidebar.tsx
+++ components/shared/LeftSidebar.tsx
@@ -1,5 +1,18 @@
+import { sidebarLinks} from "@/constants";
+
 function LeftSidebar() {
-    return <h1>LeftSidebar</h1>
+    return (
+        <section className="custom-scrollbar leftsidebar">
+            <div className="flex w-full flex-1 flex-col gap-6 px-6">
+                {sidebarLinks.map((link) => (
+                    <div>
+                        Link
+                    </div>
+                ))}
+            </div>
+
+        </section>
+    )
 }
 
 export default LeftSidebar;
