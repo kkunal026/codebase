dd393095519f743509966b1e567044bc5941ad90
kkunal026 <kunalakakk@gmail.com>
2024-03-08 16:51:31 +0530

    Card working

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,3 +1,4 @@
+import ThreadCard from "@/components/cards/ThreadCard";
 import { fetchPosts } from "@/lib/actions/thread.actions";
 import { UserButton } from "@clerk/nextjs";
 import { currentUser } from "@clerk/nextjs";
@@ -22,7 +23,7 @@ export default async function Home() {
               <ThreadCard 
                 key={post._id}
                 id={post._id}
-                currentUserId={user?.id}
+                currentUserId={user?.id || ""} 
                 parentId={post.parentId}
                 content={post.text}
                 author={post.author}
