b5dd4ca6b4064e267a3c19751471e8fcb4d977b5
kkunal026 <kunalakakk@gmail.com>
2024-03-09 12:28:42 +0530

    making the user  card

---
+++ app/(root)/activity/page.tsx
@@ -0,0 +1,21 @@
+import { fetchUser } from "@/lib/actions/user.actions";
+import { currentUser } from "@clerk/nextjs";
+import { redirect } from "next/navigation";
+
+const Page = async () => {
+  const user = await currentUser();
+  if(!user) return null;
+
+  const userInfo = await fetchUser(user.id);
+  if(!userInfo?.onboarded) redirect ('/onboarding');
+
+  
+
+    return (
+      <section>
+          <h1 className="head-text mb-10">Activity</h1>
+      </section>
+    )
+  }
+  
+  export default Page;
--- app/(root)/search/page.tsx
+++ app/(root)/search/page.tsx
@@ -1,10 +1,40 @@
+import { fetchUser, fetchUsers } from "@/lib/actions/user.actions";
+import { currentUser } from "@clerk/nextjs";
+import { redirect } from "next/navigation";
 
-const Page = () => {
-  return (
-    <section>
-        <h1 className="head-text mb-10">Search</h1>
-    </section>
-  )
-}
+const Page = async () => {
+  const user = await currentUser();
+  if(!user) return null;
 
-export default Page
+  const userInfo = await fetchUser(user.id);
+  if(!userInfo?.onboarded) redirect ('/onboarding');
+
+  const result = await fetchUsers({
+    userId: user.id,
+    searchString: '',
+    pageNumber: 1,
+    pageSize: 25
+  })
+
+    return (
+      <section>
+          <h1 className="head-text mb-10">Search</h1>
+
+          <div className="mt-14 flex flex-col gap-9">
+            {result.users.length === 0 ? (
+              <p className="no-result">No users</p>
+            ) : (
+              <>
+                {result.users.map((person) => (
+                  <UserCard 
+                  
+                  />
+                ))}
+              </>
+            )} 
+          </div>
+      </section>
+    )
+  }
+  
+  export default Page;
