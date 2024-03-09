ed5780b4795ffc31f8a9ca5a2f868d0dfd1080a4
kkunal026 <kunalakakk@gmail.com>
2024-03-10 01:32:25 +0530

    fixed an error

--- middleware.ts
+++ middleware.ts
@@ -2,7 +2,7 @@ import { authMiddleware } from "@clerk/nextjs";
 
 export default authMiddleware({
     publicRoutes: ['/', '/api/webhook/clerk', "/api/uploadthing"],
-    ignoredRoutes: ['/api/webhook/clerk']
+    ignoredRoutes: ['/api/webhook/clerk', "/api/uploadthing"]
 });
 
 export const config = {
