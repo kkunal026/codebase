0c551560cc16093b3b971120bbd62aea2c619242
kkunal026 <kunalakakk@gmail.com>
2024-03-20 23:31:59 +0530

    middleware issue

--- middleware.ts
+++ middleware.ts
@@ -1,8 +1,19 @@
+// import { authMiddleware } from "@clerk/nextjs";
+
+// export default authMiddleware({
+//     publicRoutes: ['/', '/api/webhook/clerk', "/api/uploadthing"],
+//     ignoredRoutes: ['/api/webhook/clerk', "/api/uploadthing"]
+// });
+
+// export const config = {
+//     matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"],
+// };
+
 import { authMiddleware } from "@clerk/nextjs";
 
 export default authMiddleware({
-    publicRoutes: ['/', '/api/webhook/clerk', "/api/uploadthing"],
-    ignoredRoutes: ['/api/webhook/clerk', "/api/uploadthing"]
+    publicRoutes: ['/'],
+    ignoredRoutes: ['/api(.*)']
 });
 
 export const config = {
